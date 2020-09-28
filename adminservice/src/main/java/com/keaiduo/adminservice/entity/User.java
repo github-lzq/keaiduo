428cf737a0bb452103e4ba60b745eccc69e4917c
卢祖权 <876331434@qq.com>
2020-09-28 23:45:12 +0800

    可爱多用户模块

--- adminservice/src/main/java/com/keaiduo/adminservice/entity/User.java
+++ adminservice/src/main/java/com/keaiduo/adminservice/entity/User.java
@@ -11,14 +11,22 @@ import java.util.Date;
 public class User {
     @ApiModelProperty(value = "用户ID",required = true)
     private int uid;
-    
+    @ApiModelProperty(value = "用户名")
     private String username;
+    @ApiModelProperty(value = "密码",required = true)
     private String password;
+    @ApiModelProperty(value = "邮箱")
     private String email;
+    @ApiModelProperty(value = "手机")
     private String mobile;
+    @ApiModelProperty(value = "找回密码问题")
     private String question;
+    @ApiModelProperty(value = "找回密码答案")
     private String answer;
+    @ApiModelProperty(value = "角色0-管理员,1-普通用户")
     private int role;
+    @ApiModelProperty(value = "创建时间")
     private Date creatTime;
+    @ApiModelProperty(value = "最后一次更新时间")
     private Date updateTime;
 }
---
+++ adminservice/src/main/java/com/keaiduo/adminservice/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.keaiduo.adminservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.keaiduo.adminservice.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
